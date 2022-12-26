c83d345686b1c888eee07863275ba9f13da73c57
RiteshKk <riteshch3004@gmail.com>
2022-12-26 13:57:59 +0530

    pagingdata moved to constructor

--- app/src/main/java/ritesh/movieslistapp/presentation/MovieViewModel.kt
+++ app/src/main/java/ritesh/movieslistapp/presentation/MovieViewModel.kt
@@ -2,48 +2,20 @@ package ritesh.movieslistapp.presentation
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import androidx.paging.PagingData.Companion
 import androidx.paging.cachedIn
-import androidx.paging.compose.LazyPagingItems
-import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.filter
-import androidx.paging.flatMap
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectIndexed
-import kotlinx.coroutines.flow.count
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flatMapMerge
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.switchMap
-import kotlinx.coroutines.flow.toCollection
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import ritesh.movieslistapp.data.repository.MovieRepository
 import ritesh.movieslistapp.domain.model.Content
 import ritesh.movieslistapp.domain.paging.MoviePagingSource
@@ -54,6 +26,20 @@ import javax.inject.Inject
 class MovieViewModel @Inject constructor(
     private val repo: MovieRepository
 ) : ViewModel() {
+    private val pagingData: Flow<PagingData<Content>>
+
+    init {
+        pagingData = Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false,
+                initialLoadSize = 20
+            ),
+            pagingSourceFactory =
+            { MoviePagingSource(repo, _title, _searchTextState.value) }
+        ).flow.cachedIn(viewModelScope)
+    }
 
     private val _searchWidgetState: MutableState<SearchWidgetState> = mutableStateOf(CLOSE)
     val searchWidgetState: State<SearchWidgetState> = _searchWidgetState
@@ -72,18 +58,6 @@ class MovieViewModel @Inject constructor(
     private val _title: MutableStateFlow<String> = MutableStateFlow("")
     val title: StateFlow<String> = _title.asStateFlow()
 
-    private val pagingData = Pager(
-        config = PagingConfig(
-            pageSize = 20,
-            maxSize = 100,
-            enablePlaceholders = false,
-            initialLoadSize = 20
-        ),
-        pagingSourceFactory =
-        { MoviePagingSource(repo, _title, _searchTextState.value) }
-    ).flow.cachedIn(viewModelScope)
-
-    // lateinit var result: Flow<PagingData<Content>>
     fun filterData() = if (_searchTextState.value.length >= 3) {
         pagingData.onEach { paging ->
             paging.filter { it.name.contains(_searchTextState.value, true) }
